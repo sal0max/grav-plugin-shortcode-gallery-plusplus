f8acb71a57464111cbb2fd022fae273f17e2f8c7
Sailoriae <62501693+Sailoriae@users.noreply.github.com>
2021-09-06 16:51:08 +0200

    Let users decide their resize factor of thumbnails

--- shortcodes/GalleryPlusPlusShortcode.php
+++ shortcodes/GalleryPlusPlusShortcode.php
@@ -26,6 +26,7 @@ class GalleryPlusPlusShortcode extends Shortcode
             $lastRow = $shortcode->getParameter('lastRow', $pluginConfig['gallery']['lastRow']);
             $captions = $shortcode->getParameter('captions', $pluginConfig['gallery']['captions']);
             $border = $shortcode->getParameter('border', $pluginConfig['gallery']['border']);
+            $resizeFactor = $shortcode->getParameter('resizeFactor ', $pluginConfig['gallery']['resizeFactor']);
 
             // overwrite default lightbox settings, if set by user
             $openEffect = $shortcode->getParameter('openEffect', $pluginConfig['lightbox']['openEffect']);
@@ -96,6 +97,7 @@ class GalleryPlusPlusShortcode extends Shortcode
                 'lastRow' => $lastRow,
                 'captions' => $captions,
                 'border' => $border,
+                'resizeFactor' => $resizeFactor,
                 // lightbox settings
                 'openEffect' => $openEffect,
                 'closeEffect' => $closeEffect,
